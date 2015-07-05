fd6f3b0d036f0f85849f0393cd77063d98e1c678
Tom Booth <tombooth@gmail.com>
2015-07-05 21:22:52 +0100

    Store public and private key on the field
    We will want to use these two values to encrypt and decrypt the
    serialised data stored in the database. We will stored the armoured
    version and then use PostgreSQL to switch this into bytes.
    
    The key pair used in the test are a random pair that I created soley
    for this use.

--- jsonpgpfield/fields.py
+++ jsonpgpfield/fields.py
@@ -5,5 +5,10 @@ class JSONPGPField(models.Field):
 
     description = "JSON stored encrypted with asymetric PGP"
 
+    def __init__(self, public_key, private_key, *args, **kwargs):
+        self.public_key = public_key
+        self.private_key = private_key
+        super(JSONPGPField, self).__init__(*args, **kwargs)
+
     def db_type(self, connection):
         return 'bytea'
